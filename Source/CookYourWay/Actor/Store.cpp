368692bedce70e85e025aeac666ad06667abb743
limce106 <limce106@naver.com>
2024-09-09 03:31:01 +0900

    Add Store Actor
    - 상권 액터 추가
    - 손님을 스폰하는 CreateCustomer() 추가
    - 초를 세는 DelayWithDeltaTime()을 추가하여 임의로 5초에 한 번씩 손님이 스폰되도록 함
    - Store.cpp를 부모로 갖는 Store 블루프린트 클래스 추가

---
+++ Source/CookYourWay/Actor/Store.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/Store.h"
+#include "Customer.h"
+
+AStore::AStore()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AStore::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AStore::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (DelayWithDeltaTime(5, DeltaTime)) {
+		CreateCustomer();
+	}
+}
+
+bool AStore::DelayWithDeltaTime(float DelayTime, float DeltaSeconds)
+{
+	if (TempTime > DelayTime) {
+		TempTime = 0;
+		return true;
+	}
+	else {
+		TempTime += DeltaSeconds;
+		return false;
+	}
+}
+
+void AStore::CreateCustomer()
+{
+	FVector CustomerLocation = FVector(GetActorLocation().X, GetActorLocation().Y + 200, GetActorLocation().Z);
+	ACustomer* Customer = GetWorld()->SpawnActor<ACustomer>(BP_Customer, CustomerLocation, GetActorRotation());
+}
+
---
+++ Source/CookYourWay/Actor/Store.h
@@ -0,0 +1,30 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Store.generated.h"
+
+UCLASS()
+class COOKYOURWAY_API AStore : public AActor
+{
+	GENERATED_BODY()
+	
+	int32 TempTime;
+
+public:
+	AStore();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<APawn> BP_Customer;
+
+	virtual void Tick(float DeltaTime) override;
+
+	bool DelayWithDeltaTime(float DelayTime, float DeltaSeconds);
+	void CreateCustomer();
+
+};
