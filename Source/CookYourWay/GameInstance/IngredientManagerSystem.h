6aad09fc5661e5046115b4bd744ee0eb6f5d6109
limce106 <limce106@naver.com>
2024-09-20 02:08:10 +0900

    Add Ingredient Data
    - 재료 관련 데이터를 관리하는 IngredientManagerSystem 추가
    - 재료 테이블 구조체 추가

---
+++ Source/CookYourWay/GameInstance/IngredientManagerSystem.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "Subsystems/Subsystem.h"
+#include "IngredientManagerSystem.generated.h"
+
+USTRUCT(BlueprintType)
+struct FIngrData : public FTableRowBase {
+	GENERATED_BODY()
+
+public:
+	FIngrData() : IngrName("-1"), IngrClass("-1"), IngrType("-1"), IngrGroupCode(0) {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	FString IngrName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	FString IngrClass;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	FString IngrType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	int32 IngrGroupCode;
+	/*UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	UTexture2D* IngrIcon;*/
+};
+
+UCLASS()
+class COOKYOURWAY_API UIngredientManagerSystem : public USubsystem
+{
+	GENERATED_BODY()
+	
+};
