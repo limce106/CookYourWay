17a435e2dad44acf1a65e84c8d1afe3ffb173986
limce106 <limce106@naver.com>
2024-09-24 02:35:16 +0900

    Create Cooking Utensil Class
    - 조리 도구 클래스 생성
    - 조리 도구 클래스를 상속받는 도마, 후라이팬 블루프린트 클래스 생성
    
    * 조리 도구 블루프린트 클래스 안에 Ingredient 액터를 배치하고자 함. Ingredient 스태틱 메시가 추가되어 있는데, 이를 해당 액터로 변경 필요.

---
+++ Source/CookYourWay/Actor/CookingUtensil.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/CookingUtensil.h"
+
+// Sets default values
+ACookingUtensil::ACookingUtensil()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ACookingUtensil::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACookingUtensil::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
